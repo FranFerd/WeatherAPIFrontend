f00935e4414a849a4710f35e7159eb0163ac54bb
Vlad Solovey <vlad.solovey7@gmail.com>
2025-05-22 15:12:06 +0300

    Working on weather for week

---
+++ src/components/WeatherForDay.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>address</h1>
+    </div>
+    <div class="weather-info-container">
+        <h2>Today, 22 may</h2>
+    </div>
+</template>
+
+<style scoped>
+.weather-info-container{
+    width: 1000px;
+    height: 300px;
+    background-color: azure;
+}
+</style>
--- src/views/WeatherForWeekVIew.vue
+++ src/views/WeatherForWeekVIew.vue
@@ -1,7 +1,43 @@
 <script setup>
+import WeatherForDay from '@/components/WeatherForDay.vue';
+import axios from 'axios';
+import { onMounted, watch, ref} from 'vue';
+import { useRoute } from 'vue-router';
 
+const addressUrl = ref(null)
+const route = useRoute()
+const props = defineProps({
+    address : {
+        type: String,
+        required: true
+    }
+})
+watch(() => route.params.address, (newAddress) => {
+    addressUrl.value = newAddress
+}, {immediate:true} )
+
+async function fetchWeatherData(location){
+    try{
+        const response = await axios.get(`http://127.0.0.1:5000/weather/hourly/${location}`)
+        return response.data
+    }
+    catch(error){
+        console.error(`Error: `, error)
+        return null
+
+    }
+
+}
+
+onMounted(async() => {
+    const weatherData = await fetchWeatherData(addressUrl.value)
+    if(weatherData){
+    }
+})
 </script>
 
 <template>
-    <h1>Weather for week</h1>
+    <WeatherForDay
+
+    ></WeatherForDay>
 </template>
